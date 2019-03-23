dc9d5313ad2fafcf5c5840e4e1877543f17f5834
carlinhos1807 <karlinhos1807@gmail.com>
2019-03-23 16:37:56 -0300

    Update T1.hs
    Questões 1, 2 e 3

---
+++ T1/T1.hs
@@ -0,0 +1,33 @@
+--1.Crie uma função isVowel :: Char -> Bool que verifique se um caracter é uma vogal ou não.
+
+isVowel :: Char -> Bool
+isVowel c
+      | c == 'a' = True
+      | c == 'e' = True
+      | c == 'i' = True
+      | c == 'o' = True
+      | c == 'u' = True
+      | c == 'A' = True
+      | c == 'E' = True
+      | c == 'I' = True
+      | c == 'O' = True
+      | c == 'U' = True
+      | otherwise = False
+
+--2.Escreva uma função addComma, que adicione uma vírgula no final de cada string contida numa lista.
+
+addComma :: [String] -> [String]
+addComma s = map(\p -> p ++ ",")s
+
+--3.Crie uma função htmlListItems :: [String] -> [String]
+--que receba uma lista de strings e retorne outra lista contendo as strings formatadas como itens de lista em HTML.
+--Com funcao anonima:
+
+htmlListItems :: [String] -> [String]
+htmlListItems l = map(\p -> "<LI>" ++ p ++ "<LI>")l
+
+--Sem funcao anonima:
+formata :: [String] -> [String]
+formata s = map(\p -> "<LI>" ++ p ++ "<LI>")s
+htmlListItems2 :: [String] -> [String]
+htmlListItems2 l = formata l
