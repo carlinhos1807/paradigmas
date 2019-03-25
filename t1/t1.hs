8b281cab9ccea5f9ff66eb195e324f89423f4731
carlinhos1807 <48457630+carlinhos1807@users.noreply.github.com>
2019-03-25 18:54:44 -0300

    Rename T1/T1.hs to t1/t1.hs

--- t1/t1.hs
+++ t1/t1.hs
@@ -1,6 +1,6 @@
 import Data.Char
 
---1.Crie uma função isVowel :: Char -> Bool que verifique se um caracter é uma vogal ou não.
+--1.Crie uma funÃ§Ã£o isVowel :: Char -> Bool que verifique se um caracter Ã© uma vogal ou nÃ£o.
 
 isVowel :: Char -> Bool
 isVowel c
@@ -16,12 +16,12 @@ isVowel c
       | c == 'U' = True
       | otherwise = False
 
---2.Escreva uma função addComma, que adicione uma vírgula no final de cada string contida numa lista.
+--2.Escreva uma funÃ§Ã£o addComma, que adicione uma vÃ­rgula no final de cada string contida numa lista.
 
 addComma :: [String] -> [String]
 addComma s = map(\p -> p ++ ",")s
 
---3.Crie uma função htmlListItems :: [String] -> [String]
+--3.Crie uma funÃ§Ã£o htmlListItems :: [String] -> [String]
 --que receba uma lista de strings e retorne outra lista contendo as strings formatadas como itens de lista em HTML.
 
 --Com funcao anonima:
@@ -36,7 +36,7 @@ formata s = map(\p -> "<LI>" ++ p ++ "<LI>")s
 htmlListItems2 :: [String] -> [String]
 htmlListItems2 l1 = formata l1
 
---4.Defina uma função que receba uma string e produza outra retirando as vogais.
+--4.Defina uma funÃ§Ã£o que receba uma string e produza outra retirando as vogais.
 
 --Com funcao anonima:
 
@@ -50,7 +50,7 @@ filtraVog s1 = filter(\p -> p /= 'a' && p /= 'e'  && p /= 'i'  && p /= 'o'  && p
 delvowel2 :: String -> String
 delvowel2 s = filtraVog s
 
---5.Defina uma função que receba uma string, possivelmente contendo espaços, e que retorne outra string substituindo os demais caracteres por '-', mas mantendo os espaços.
+--5.Defina uma funÃ§Ã£o que receba uma string, possivelmente contendo espaÃ§os, e que retorne outra string substituindo os demais caracteres por '-', mas mantendo os espaÃ§os.
 
 --Com funcao anonima:
 
@@ -64,25 +64,25 @@ substitui c = map(\p -> if p /= ' ' then '-' else p)c
 codifica2 :: String -> String
 codifica2 s = substitui s
 
---6.Escreva uma função firstName :: String -> String que, dado o nome completo de uma pessoa, obtenha seu primeiro nome. 
+--6.Escreva uma funÃ§Ã£o firstName :: String -> String que, dado o nome completo de uma pessoa, obtenha seu primeiro nome. 
 
 firstName :: String -> String
 firstName = takeWhile (/=' ')
 
---7.Escreva uma função isInt :: String -> Bool que verifique se uma dada string só contém dígitos de 0 a 9. Exemplos
+--7.Escreva uma funÃ§Ã£o isInt :: String -> Bool que verifique se uma dada string sÃ³ contÃ©m dÃ­gitos de 0 a 9. Exemplos
 
 verTam :: Int -> Int -> Bool
 verTam t1 t2 = (t1+t2) == 0
 isInt :: String -> Bool
 isInt s1 = verTam (length((filter(`elem` ['A'..'Z'])s1))) (length((filter(`elem` ['a'..'z'])s1)))
 
---8.Escreva uma função lastName :: String -> String que, dado o nome completo de uma pessoa, obtenha seu último sobrenome.
+--8.Escreva uma funÃ§Ã£o lastName :: String -> String que, dado o nome completo de uma pessoa, obtenha seu Ãºltimo sobrenome.
 
 lastName :: String -> String
 lastName n = last(words n)
 
---9.Escreva uma função userName :: String -> String que, dado o nome completo de uma pessoa, crie um nome de usuário (login) da pessoa,
---formado por: primeira letra do nome seguida do sobrenome, tudo em minúsculas.
+--9.Escreva uma funÃ§Ã£o userName :: String -> String que, dado o nome completo de uma pessoa, crie um nome de usuÃ¡rio (login) da pessoa,
+--formado por: primeira letra do nome seguida do sobrenome, tudo em minÃºsculas.
 
 toMaius :: String -> String
 toMaius n = map(toLower)n
@@ -90,14 +90,14 @@ userName :: String -> String
 userName s = toMaius (showLitChar (head s) (last(words s)))
 
 
---10.Escreva uma função encodeName :: String -> String que substitua vogais em uma string, conforme o esquema a seguir: a = 4, e = 3, i = 2, o = 1, u = 0.
+--10.Escreva uma funÃ§Ã£o encodeName :: String -> String que substitua vogais em uma string, conforme o esquema a seguir: a = 4, e = 3, i = 2, o = 1, u = 0.
 
 encodeName :: String -> String
 encodeName s1 = map(\p -> if (p == 'u' || p == 'U') then '0' else if (p == 'o' || p == 'O') then '1' else if (p == 'i' || p == 'I')then '2' 
 else if (p == 'E' || p == 'e') then '3' 
 else if (p == 'a' || p == 'A') then '4' else p)s1
 
---11.Escreva uma função betterEncodeName :: String -> String que substitua vogais em uma string, conforme este esquema: a = 4, e = 3, i = 1, o = 0, u = 00. 
+--11.Escreva uma funÃ§Ã£o betterEncodeName :: String -> String que substitua vogais em uma string, conforme este esquema: a = 4, e = 3, i = 1, o = 0, u = 00. 
 
 betterEncodeName :: String -> String
 betterEncodeName s1 = concatMap(\p -> if (p == 'u' || p == 'U') then "00" else if (p == 'o' || p == 'O') then "1" else if (p == 'i' || p == 'I')then "2" 
@@ -105,7 +105,7 @@ else if (p == 'E' || p == 'e') then "3"
 else if (p == 'a' || p == 'A') then "4" else [p])s1
 
 --12.Dada uma lista de strings, produzir outra lista com strings de 10 caracteres, 
---usando o seguinte esquema: strings de entrada com mais de 10 caracteres são truncadas, strings com até 10 caracteres são completadas com '.' até ficarem com 10 caracteres.
+--usando o seguinte esquema: strings de entrada com mais de 10 caracteres sÃ£o truncadas, strings com atÃ© 10 caracteres sÃ£o completadas com '.' atÃ© ficarem com 10 caracteres.
 
 concatena :: String -> String
 concatena s = s ++ (take (10 - (length s)) "..........")
